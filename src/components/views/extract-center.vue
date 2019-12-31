e65a7cf7f23f7b95568bb87358de951deca02100
zhuqiu <zhuqiu@authine.com>
2019-12-31 10:33:57 +0800

    增加提现模块

---
+++ src/components/views/extract-center.vue
@@ -0,0 +1,37 @@
+<template>
+	<div>
+    <div class="extract-center">
+      <van-nav-bar
+        title="提现中心"
+        left-text="返回"
+        left-arrow
+        @click-left="onClickLeft"
+      />
+      
+    </div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'recharge-center',
+	data() {
+		return {
+
+		}
+  },
+  mounted(){
+    console.log(this.$route.params.num)
+  },
+	methods:{
+    onClickLeft(){
+      this.$router.push({name: 'Index'})
+    }
+	}
+
+}
+</script>
+
+<style>
+
+</style>
